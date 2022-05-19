9e9e4c6e154b8991cdaf7baa016201a69cf5960f
Henri Podolski <HenriPodolski@users.noreply.github.com>
2022-05-19 17:35:03 +0200

    Update publish.sh

--- src/publish.sh
+++ src/publish.sh
@@ -9,7 +9,7 @@ echo "OVERRIDE=$OVERRIDE"
 docker login ghcr.io -u ${GITHUB_REF} -p ${REPO_TOKEN}
 
 VERSION=$VERSION docker-compose -f $OVERRIDE --env-file $ENV_FILE up --no-start --remove-orphans
-IMAGES=$(docker-compose -f $DOCKER_COMPOSE --env-file $ENV_FILE images -q)
+IMAGES=$(docker-compose -f $OVERRIDE --env-file $ENV_FILE images -q)
 
 echo ".env file:"
 cat .env
