896b55f19dd5ecc3b9ac132d2c8677c53ad85cd5
Henri Podolski <HenriPodolski@users.noreply.github.com>
2022-05-19 15:45:00 +0200

    Update publish.sh

--- src/publish.sh
+++ src/publish.sh
@@ -11,6 +11,8 @@ docker login ghcr.io -u ${GITHUB_REF} -p ${REPO_TOKEN}
 docker-compose -f docker-compose.yml -f $OVERRIDE --env-file $ENV_FILE up --no-start --remove-orphans
 IMAGES=$(docker inspect --format='{{.Image}}' $(docker ps -aq))
 
+echo ".env file:"
+cat .env
 echo "IMAGES: $IMAGES"
 for IMAGE in $IMAGES; do
     echo "IMAGE: $IMAGE"
